22d5c491f370e4d1567bcb565ed9cd959fb46aac
Tobi Bosede <abosede2@jhu.edu>
2016-01-06 20:21:06 -0500

    added swirl commands
    also removed some irrelevant code

--- swirl setup.R
+++ swirl setup.R
@@ -5,9 +5,10 @@ install_from_swirl("R Programming Alt")
 install_from_swirl("Mathematical Biostatistics Boot Camp")
 swirl()
 
-
-#create custom course
-devtools::install_github(c("swirldev/swirl", "swirldev/swirlify"))
-library(swirlify)
-
-#When you are at the R prompt (>):| -- Typing skip() allows you to skip the current question.| -- Typing play() lets you experiment with R on your own; swirl will ignore what you do...| -- UNTIL you type nxt() which will regain swirl's attention.| -- Typing bye() causes swirl to exit. Your progress will be saved.| -- Typing main() returns you to swirl's main menu.| -- Typing info() displays these options again.
+#important swirl commands
+'skip() - move on
+play() - swirl ignores
+nxt() - stop playing
+bye()/0 - exit
+info() - display commands
+... - prompt to hit enter'
